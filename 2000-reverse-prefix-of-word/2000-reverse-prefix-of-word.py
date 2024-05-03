0af39671107f460052def87259d5b88250f207b8
Harini <63025647+harinik05@users.noreply.github.com>
2024-05-02 22:57:42 -0400

    Time: 12 ms (71.42%), Space: 11.6 MB (92.57%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -1,19 +1,15 @@
-class Solution:
-    def reversePrefix(self, word: str, ch: str) -> str:
-        #1. Convert into a list 
-        wordList = list(word)
-        outputWord = ""
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        #1. Initialize the return string
+        returnStr= word
+
+        #2. Reverse the string
+        for i in range(len(word)):
+            if word[i] == ch:
+                returnStr = ''.join(reversed(word[0:i+1]))
+                returnStr = returnStr+word[i+1:]
+                return returnStr
+
+        #3. return the final answer
+        return returnStr
         
-        #2. determine the index where ch is there
-        try:
-            indexPresent = wordList.index(ch)
-        except ValueError:
-            return word
-        
-        #3. check if you can find out to reverse it
-        if indexPresent!=-1:
-            outputWord = ''.join(reversed(wordList[0:indexPresent+1]))
-            outputWord += ''.join(wordList[indexPresent+1:])
-        
-        #4. return the output word
-        return outputWord
