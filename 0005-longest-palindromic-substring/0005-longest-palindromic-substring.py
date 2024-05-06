1484e6ff18277cbb22dd4edae6479a32e3879268
Harini <63025647+harinik05@users.noreply.github.com>
2024-05-06 10:21:55 -0400

    Time: 2924 ms (26.57%), Space: 25.2 MB (7.25%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,13 +1,26 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        longestSoFar = ""
-        # for loop 
+        # Create a matrix to store the dp values
+        dpMatrix = [[False for _ in range(len(s))] for _ in range(len(s))]
+        longest_palindrome = ""
+
+        # Base case: single characters are palindromes
         for i in range(len(s)):
-            for j in range(i,len(s)):
-                temp = s[i:j+1]
-                #a. condition for its a palindrome
-                if temp == temp[::-1] and len(temp) > len(longestSoFar):
-                    longestSoFar = temp
-                
-        return longestSoFar
-                
+            dpMatrix[i][i] = True
+            longest_palindrome = s[i]
+
+        # Base case: two consecutive same characters are palindromes
+        for i in range(len(s) - 1):
+            if s[i] == s[i + 1]:
+                dpMatrix[i][i + 1] = True
+                longest_palindrome = s[i:i + 2]
+
+        # General case: for substrings of length > 2
+        for length in range(3, len(s) + 1):
+            for i in range(len(s) - length + 1):
+                j = i + length - 1
+                if s[i] == s[j] and dpMatrix[i + 1][j - 1]:
+                    dpMatrix[i][j] = True
+                    longest_palindrome = s[i:j + 1]
+
+        return longest_palindrome
