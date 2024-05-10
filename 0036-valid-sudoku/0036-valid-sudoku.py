8fecacef938724ff79227456eb6a9157d742d6c9
Harini <63025647+harinik05@users.noreply.github.com>
2024-05-10 08:46:59 -0400

    Time: 99 ms (21.00%), Space: 16.6 MB (63.51%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     # Constructor
     def __init__(self):
@@ -5,29 +7,30 @@ class Solution:
         self.rows = [set() for _ in range(self.n)]
         self.columns = [set() for _ in range(self.n)]
         self.boxes = [set() for _ in range(self.n)]
-
+    
     # Valid sudoku
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         for r in range(self.n):
             for c in range(self.n):
+                # Value of the board
                 value = board[r][c]
+                if value == ".":
+                    continue
+                
+                # Values in rows
+                if value in self.rows[r]:
+                    return False
+                self.rows[r].add(value)
 
-                # Check if the value is a digit
-                if value.isdigit():
-                    # Check rows
-                    if value in self.rows[r]:
-                        return False
-                    self.rows[r].add(value)
+                # Values in columns
+                if value in self.columns[c]:
+                    return False
+                self.columns[c].add(value)
 
-                    # Check columns
-                    if value in self.columns[c]:
-                        return False
-                    self.columns[c].add(value)
-
-                    # Check boxes
-                    boxIndex = (r // 3) * 3 + (c // 3)
-                    if value in self.boxes[boxIndex]:
-                        return False
-                    self.boxes[boxIndex].add(value)
-
+                # Values in boxes
+                boxIndex = (r // 3) * 3 + (c // 3)
+                if value in self.boxes[boxIndex]:
+                    return False
+                self.boxes[boxIndex].add(value)
+        
         return True
