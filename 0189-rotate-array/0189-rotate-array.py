62e2e3ad8bbee49ba9f23e82f53c4c786d08ace5
Harini <63025647+harinik05@users.noreply.github.com>
2024-05-19 12:56:03 -0400

    Time: 163 ms (44.37%), Space: 28 MB (35.20%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -1,8 +1,24 @@
 class Solution:
+    def reverse(self, nums, start, end):
+        while start<end:
+            nums[start], nums[end] = nums[end],nums[start]
+            start+=1
+            end-=1
+            
     def rotate(self, nums: List[int], k: int) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n = len(nums)
+        k%=n
+        #1. reverse the entire thing
+        self.reverse(nums,0,n-1)
+        self.reverse(nums,0,k-1)
+        self.reverse(nums,k,n-1)
+        
+       
+        
+        '''
         # First Solution - Not in place extra memory
         result = [0] * len(nums)  
         
@@ -11,6 +27,7 @@ class Solution:
             result[indexToInsert] = nums[i]
         
         nums[:] = result
+        '''
             
             
         
