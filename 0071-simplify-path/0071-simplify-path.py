13b7e83dc43e0437df96478299292bfaae6ea0f1
Harini <63025647+harinik05@users.noreply.github.com>
2024-05-26 09:42:48 -0400

    Time: 42 ms (24.17%), Space: 16.6 MB (85.41%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.py
@@ -0,0 +1,21 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        #1. Initiate a stack 
+        stack = []
+        
+        #2. String.split method 
+        splitMethod = path.split("/")
+        
+        #3. Loop through all of them and put it in a stack 
+        for i in range(len(splitMethod)):
+            if splitMethod[i] == "..":
+                if stack:
+                    stack.pop()
+            elif splitMethod[i] != "." and splitMethod[i] != "":
+                stack.append(splitMethod[i])
+        
+        #4. concatenate everything in the stack 
+        finalStr = "/" + "/".join(stack)
+        
+        #5. return the string
+        return finalStr if finalStr else "/"
