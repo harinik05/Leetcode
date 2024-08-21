e5396fb1505ab4a4cb489c57bcb55c60c218e325
Harini <63025647+harinik05@users.noreply.github.com>
2024-08-21 17:00:41 -0400

    Time: 4 ms (14.59%), Space: 45.1 MB (11.80%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,6 +1,49 @@
 class Solution {
     public int currentArea = 0;
-    public int maxAreaOfIsland(int[][] grid) {
+    Queue<int[]> queue = new LinkedList<>();
+    Queue<int[]> DFSqueue = new LinkedList<>();
+    public int[][] directions = new int[][]{{1,0},{0,1},{-1,0}, {0,-1}};
+
+    public int DFSArea = 0;
+    public int maxAreaOfIsland(int[][] grid){
+        int maxArea = 0;
+        //1. go through all elements and call BFS
+        for(int i = 0;i<grid.length;i++){
+            for(int j= 0;j<grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    this.DFSArea = 0;
+                    this.BFSFunction(grid, i, j);
+                    maxArea = Math.max(maxArea, this.DFSArea);
+                    
+                }
+            }
+        }
+        return maxArea;
+        
+    }
+    
+    public void BFSFunction(int[][] grid, int r, int c){
+        //1. Put some stuff in the queue 
+        DFSqueue.offer(new int[]{r,c});
+        grid[r][c] = 0;
+        this.DFSArea++;
+        //2. While queue isnt empty, pop the element 
+        while(!this.DFSqueue.isEmpty()){
+            int[] current = this.DFSqueue.poll();
+            for(int[] direction: this.directions){
+                int newR = current[0]+ direction[0];
+                int newC = current[1] + direction[1];
+                
+                if((newR>=0) && (newC>=0) &&(newR<grid.length) &&(newC<grid[0].length)&& (grid[newR][newC] == 1)){
+                    this.DFSqueue.offer(new int[]{newR,newC});
+                    grid[newR][newC] = 0;
+                    this.DFSArea++;
+                }
+            }
+        }
+        //3. Loop through all the directions and queue (append if valid )
+    }
+    public int maxAreaOfIslandDFS(int[][] grid) {
         //1. Initiate the maximum counter 
         int maxOutput = 0;
         for(int i = 0;i<grid.length;i++){
