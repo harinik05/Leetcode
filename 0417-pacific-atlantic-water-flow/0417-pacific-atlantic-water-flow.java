0d1b26affc52cce2c07d2358c6d96a617d714aad
Harini <63025647+harinik05@users.noreply.github.com>
2024-08-21 20:51:11 -0400

    Time: 6 ms (47.86%), Space: 45.8 MB (36.97%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,8 +1,84 @@
 class Solution {
     public boolean[][] pBool;
     public boolean[][] aBool;
+    public int[][] directions = new int[][]{{-1,0},{1,0},{0,1},{0,-1}};
+    public Queue<int[]> pQueue = new LinkedList<>();
+    public Queue<int[]> aQueue = new LinkedList<>();
     
-    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+    public List<List<Integer>> pacificAtlantic(int[][] heights){
+        //1. Intialize the variables 
+        int rows = heights.length;
+        int cols = heights[0].length;
+        
+        this.pBool = new boolean[rows][cols];
+        this.aBool = new boolean[rows][cols];
+        
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
+                this.pBool[i][j]=false;
+                this.aBool[i][j]= false;
+            }
+        }
+        //2. horizontal stuff
+        for(int h = 0;h<cols;h++){
+            this.BFSFunction(heights, 0, h, true);
+            this.BFSFunction(heights, rows-1, h, false);
+        }
+        
+        //3. vertical stuff
+        for(int v = 0;v<rows;v++){
+            this.BFSFunction(heights, v, 0, true);
+            this.BFSFunction(heights, v, cols-1, false);
+        }
+        
+        
+        
+        //4. Find the overlaps 
+        List<List<Integer>> output = new ArrayList<>();
+        for(int i = 0;i<rows;i++){
+            for(int j = 0;j<cols;j++){
+                if(pBool[i][j] == true && aBool[i][j] == true){
+                    output.add(Arrays.asList(i,j));
+                }
+            }
+        }
+        
+        return output;
+    
+    }
+    
+    public void BFSFunction(int[][] heights, int r, int c, boolean oceanFlag){
+        //1. Initialize the queue 
+        Queue<int[]> queue;
+        boolean[][] bArr;
+        if (oceanFlag == true){
+            queue = pQueue;
+            bArr = pBool;
+            
+        }else{
+            queue = aQueue;
+            bArr = aBool;
+        }
+        
+        queue.offer(new int[]{r,c});
+        bArr[r][c] = true;
+        //2. While the queue is not empty (current is popped off)
+        while(!queue.isEmpty()){
+            int[] current = queue.poll();
+            for(int[] direction: this.directions){
+                int newRow = current[0]+ direction[0];
+                int newCol = current[1]+ direction[1];
+                
+                if((newRow>=0) &&(newRow<heights.length)&&(newCol>=0) &&(newCol<heights[0].length) &&(heights[current[0]][current[1]]<=heights[newRow][newCol]) &&(bArr[newRow][newCol] == false)){
+                    queue.offer(new int[]{newRow,newCol});
+                    bArr[newRow][newCol]=true;
+                }
+            }
+        }
+        
+        //3. Loop through all the directions and check the conditions and add to the queue accoridngly
+    }
+    public List<List<Integer>> pacificAtlanticBFS(int[][] heights) {
         int rows = heights.length;
         int cols = heights[0].length;
         this.pBool = new boolean[rows][cols];
