a029e3780d99b7f8a35f1c0d5639af454f310a69
Harini <63025647+harinik05@users.noreply.github.com>
2024-08-23 15:33:37 -0400

    Time: 26 ms (61.61%), Space: 42.7 MB (56.25%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -21,9 +21,46 @@ class Node {
 class Solution {
     
     public Map<Node, Node> hashmap = new HashMap<>();
+    public Queue<Node> queue = new LinkedList<>();
     
+    //We'll then do the breath first search solution 
+    public Node cloneGraph(Node node){
+        if(node == null){
+            return null;
+        }
+        return this.BFSFunction(node);
+    }
+    
+    public Node BFSFunction(Node node){
+        //1. Put something in the queue 
+        this.queue.add(node);
+        Node duplicateNode = new Node(node.val);
+        this.hashmap.put(node, duplicateNode );
+        
+        //2. While the queue is not emoty 
+        while(!this.queue.isEmpty()){
+            //3. Current 
+            Node current = this.queue.poll();
+            
+            //4. Go through the neighbors 
+            for(Node neighbors: current.neighbors){
+                //check the condition 
+                if(!this.hashmap.containsKey(neighbors)){
+                    Node nDuplicate = new Node(neighbors.val);
+                    this.hashmap.put(neighbors, nDuplicate);
+                    this.queue.add(neighbors);
+                    
+                }
+                //you have to add things to the hashmap neighbors no matter what 
+                this.hashmap.get(current).neighbors.add(this.hashmap.get(neighbors));
+            }
+            
+        }
+        return this.hashmap.get(node);
+        
+    }
     //we start off with the Depth first search solution 
-    public Node cloneGraph(Node node) {
+    public Node cloneGraphDFS(Node node) {
         if(node == null){
             return null;
         }
