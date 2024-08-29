7d7e3874ecace7fa0aedc70f21b419359c34e38f
Harini <63025647+harinik05@users.noreply.github.com>
2024-08-28 21:52:13 -0400

    Time: 24 ms (44.64%), Space: 11.9 MB (20.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -19,17 +19,17 @@ class Solution(object):
         #3. While loop 
         while l<r:
             mid = (l+r)//2
-            
-            #b. compare the mid with the surrounding
-            if nums[mid-1]>nums[mid]:
-                return nums[mid]
-            elif nums[mid]>nums[mid+1]:
-                return nums[mid+1]
              #a. mid is too big?
             if nums[mid]>nums[0]:
                 l = mid+1
             else:
                 r = mid-1
+            #b. compare the mid with the surrounding
+            if nums[mid-1]>nums[mid]:
+                return nums[mid]
+            elif nums[mid]>nums[mid+1]:
+                return nums[mid+1]
+            
              
            
             
