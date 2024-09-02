c1e17e9b94f657db677b8e9723c2b106ee37b82d
Harini <63025647+harinik05@users.noreply.github.com>
2024-09-02 18:45:57 -0400

    Time: 1 ms (90.78%), Space: 47.5 MB (62.05%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,13 +1,53 @@
 class Solution {
     //constructor
     public boolean[] visited;
+    public Queue<Integer> queue = new LinkedList<>();
     public Solution(){
         
         //ArrayList<Boolean> visited = new ArrayList<>();
         //Queue<Integer> queue = new Queue<>();
         
     }
-    public int findCircleNum(int[][] isConnected) {
+    
+    public int findCircleNum(int [][] isConnected){
+        int m = isConnected.length;
+        this.visited = new boolean[m];
+        int counter = 0;
+        for(int i = 0;i<m;i++){
+            if(this.visited[i] == false){
+                counter++;
+                this.bfs(i, isConnected);
+                
+            }
+        }
+        
+        return counter;
+        
+    }
+    
+    public void bfs(int i, int[][] isConnected){
+        //1. Put the first element in the queue 
+        this.queue.offer(i);
+        this.visited[i] = true;
+        
+        //2. If the queue is still containing stuff
+        while(!this.queue.isEmpty()){
+            //a. Pop the first element 
+            int firstElem = this.queue.poll();
+            
+            //b. Loop rhrough the neighbots 
+            for(int j = 0;j<isConnected[firstElem].length;j++){
+                //1. check if theres connction and its not visited 
+                if((isConnected[firstElem][j] == 1) &&(this.visited[j] == false)){
+                    this.queue.add(j);
+                    this.visited[j] = true;
+                }
+            }
+        }
+        
+    }
+    
+    public int findCircleNumDFS(int[][] isConnected) {
         //1. Determine the length (number of cities)
         
         int m = isConnected.length;
