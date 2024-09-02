e09da29e7b6564f313f0429b16d99a39639f808d
Harini <63025647+harinik05@users.noreply.github.com>
2024-09-02 18:05:13 -0400

    Time: 175 ms (44.24%), Space: 12.1 MB (86.27%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,7 +1,36 @@
 class Solution(object):
     def __init__(self):
         self.visited = []
+        self.queue = deque()
+        
     def findCircleNum(self, isConnected):
+        self.visited = [0]*len(isConnected)
+        counter = 0
+        for i in range(len(isConnected)):
+            if self.visited[i] == 0:
+                counter+=1
+                self.bfs(i, isConnected)
+        return counter
+    
+    def bfs(self, i, isConnected):
+        #1. Put the first element in the queue 
+        self.queue.append(i)
+        self.visited[i] = 1
+        
+        #2. While queue exists 
+        while self.queue:
+            #a. Retrieve the first element 
+            firstElem = self.queue.popleft()
+            
+            #b. Go through the neighbors 
+            for k in range(len(isConnected[firstElem])):
+                if isConnected[firstElem][k] == 1 and self.visited[k]==0:
+                    self.queue.append(k)
+                    self.visited[k] = 1
+        
+                    
+    
+    def findCircleNumDFS(self, isConnected):
         """
         :type isConnected: List[List[int]]
         :rtype: int
