7188b62beee2d2e7eb650c5911e330750e059ab5
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-08 12:25:57 -0400

    Time: 371 ms (62.07%), Space: 18.7 MB (17.55%) - LeetHub

---
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -0,0 +1,53 @@
+import heapq
+# Dijstra's Algorithm
+class Solution:
+    def __init__(self):
+        self.adjacencyList = defaultdict(list)
+        self.minHeap = []
+        self.visitedSet = defaultdict(int)
+        
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        #1. Create the adjacency list for this problem 
+        for time in times:
+            # Initialize everything in here
+            ui, vi, wi  = time 
+            # Put in adjacency list 
+            self.adjacencyList[ui].append((vi, wi))
+        
+        #2. Initialize the min heap 
+        #3. Call the BFS function 
+        return self.dijstra(n,k)
+        
+    def dijstra(self, n,k):
+        #1. Initialize the Min Heap 
+        minTime = 0
+        heapq.heappush(self.minHeap, (0, k))
+        
+        #2. While min heap 
+        while self.minHeap:
+            #a. Pop the first element 
+            poppedElement = heapq.heappop(self.minHeap)
+            time, node = poppedElement
+            
+            if self.visitedSet[node] != 1:
+                # Mark as visited here once you unpacked
+                self.visitedSet[node] = 1
+                minTime = time
+                #b. Go through all the neighbors 
+                for neighbors in self.adjacencyList[node]:
+                    v1,w1 = neighbors
+                    if self.visitedSet[v1] == 0:
+                        heapq.heappush(self.minHeap, (w1+ time, v1))
+        
+        #3. Check if all nodes were reached
+        if len(self.visitedSet) == n:
+            return minTime
+        return -1
+        
+        
+                    
+                
+        
+        
+        
+        
