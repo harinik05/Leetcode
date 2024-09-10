4611b211d01370178beb80ce0effd50c008b007a
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-09 20:35:31 -0400

    Time: 51 ms (78.21%), Space: 16.7 MB (56.42%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -0,0 +1,28 @@
+from typing import List 
+
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        
+        returnSet = set()
+        counter = 0
+        for email in emails:
+            #1. Determine the local and domain name 
+            local, domain = email.split("@")
+            
+            #2. Check if there is a . then just remove it 
+            if "." in local:
+                dots = local.split(".")
+                local = "".join(dots)
+            
+            #3. Check if there is a + sign then ignore the rest 
+            if "+" in local:
+                plus = local.split("+")
+                local = plus[0]
+            
+            #4. Once filtering is done put it in a set
+            fullEmail = f"{local}@{domain}"
+            returnSet.add(fullEmail)
+            
+        
+        return len(returnSet) 
+        
