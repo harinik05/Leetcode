2fa91a39d0837366597d37bc1f8d5ee49bbcfe81
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-12 09:49:18 -0400

    Time: 44 ms (47.28%), Space: 16.8 MB (34.85%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -26,11 +26,11 @@ class Solution:
             elif s[i] in hashmapS.keys():
                 if not (hashmapS[s[i]] == t[i]):
                     return False
-                #hashmapT[t[i]] = s[i]
+                hashmapT[t[i]] = s[i]
             elif t[i] in hashmapT.keys():
                 if not(hashmapT[t[i]] == s[i]):
                     return False
-                #hashmapS[s[i]] = t[i]
+                hashmapS[s[i]] = t[i]
             
             
             #c. Not in the hashmap? then add to both 
