b1a67522fc4c287632816696453419bb4e5ed2cc
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-16 22:09:57 -0400

    Time: 288 ms (14.16%), Space: 25.8 MB (38.13%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,5 +1,28 @@
 class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+    
+    def productExceptSelf(self, nums: List[int])-> List[int]:
+        #1. Initialize the data structures
+        answerArr = [1] * len(nums)
+        
+        #2. Fill out the prefixes first 
+        for i, number in enumerate(nums):
+            if i-1<0:
+                answerArr[i] = 1
+            else:
+                answerArr[i] = answerArr[i-1] * nums[i-1]
+        
+        
+        #3. Fill out the suffixes now 
+        suffix = 1
+        for i in range(len(nums)-1,-1,-1):
+            if i+1<len(nums):
+                suffix*=nums[i+1]
+                answerArr[i]*=suffix
+        
+        return answerArr
+    
+    # Solution that contains O(n) space complexity
+    def productExceptSelf_WITHSPACE(self, nums: List[int]) -> List[int]:
         
         #1. Define my necessary data structures
         prefixArr = [1]*len(nums)
