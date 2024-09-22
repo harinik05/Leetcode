fb60e3d019ae7422172a45abf14796a31c58fc98
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-22 12:33:15 -0400

    Time: 633 ms (19.11%), Space: 23 MB (40.94%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -1,50 +1,32 @@
-from collections import defaultdict
-import math 
-
-class Solution(object):
-    def kClosest(self, points, k):
-        """
-        :type points: List[List[int]]
-        :type k: int
-        :rtype: List[List[int]]
-        """
-        #1. create a hashmap 
-        hm = defaultdict(list)
-        distance = 0
-        
-        #2. loop through everything 
-        for i in range(len(points)):
-            #a. temp array 
-            temp = points[i]
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # Find the closest values -> smallest values means min heap 
+        #1. Create a hashmap and store all the elements to it 
+        hashmap = defaultdict(int)
+        minHeap = []
+        for coordinates in points:
+            #a. Calculate the distance
+            x,y= coordinates
             
-            #b. calculate the distance 
-            distance = math.sqrt((temp[0]**2) + (temp[1]**2))
+            #b. distance
+            dist = math.sqrt(x**2 + y**2)
             
-            #c. put everything in the hashmap
-            hm[distance].append(temp)
+            # put into the hashmap 
+            minHeap.append((dist, (x,y)))
             
-
-        
-    
-        
-        #3. sort the hashmap
-        sorted_by_keys = sorted(hm.items())
-        
-        #4. pick the first k items 
-        outputArr = []
-        counter = 0
-        for distanceCalculated, listOfPoints in sorted_by_keys:
-            for point in listOfPoints:
-                outputArr.append(point)
-                counter+=1
-                if counter==k:
-                    break
-            if counter == k:
-                break
-                
-        
-        return outputArr
+        #2. Create the min heap 
+        heapq.heapify(minHeap)
+        output = []
+        #3. Loop through k elements 
+        for _ in range(k):
+            #a. Smallest values 
+            #smallestValue = heapq.heappoll(minHeap)[1]
+            smallestValue = heapq.heappop(minHeap)
             
-            
+            #b. and put in array 
+            output.append(list(smallestValue)[1])
             
         
+        return output
+        
+  
