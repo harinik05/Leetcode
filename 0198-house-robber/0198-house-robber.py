59cf0a8017ce6ef535472a4c5427e44589974e39
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-22 19:12:32 -0400

    Time: 26 ms (97.32%), Space: 16.5 MB (27.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,5 +1,28 @@
 class Solution:
-    def rob(self, nums: List[int]) -> int:
+    def __init__(self):
+        self.memo = []
+    
+    #Memoization method:
+    def memoize(self, i, nums):
+        #1. Base Case if i == 0
+        if i==0:
+            return nums[0]
+        elif i==1:
+            return max(self.memoize(0,nums), nums[1])
+        
+        #2. If i not in memo
+        if self.memo[i] == -1:
+            self.memo[i] = max(self.memoize(i-1, nums), self.memoize(i-2, nums) + nums[i])
+        
+        #3. Return the answer
+        return self.memo[i]
+            
+    def rob(self, nums:List[int]) -> int:
+        self.memo = [-1] *len(nums)
+        #1. Memoize
+        return self.memoize(len(nums)-1, nums)
+        
+    def rob_DP(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
         #a. Create the DP array 
