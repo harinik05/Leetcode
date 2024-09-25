d333e9edd8103deefb4e063559a92ab6aaa9e6f1
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-24 22:00:09 -0400

    Time: 689 ms (30.03%), Space: 33.9 MB (36.87%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -6,23 +6,26 @@ class Solution(object):
         :rtype: int
         """
         
-        #1. Initialize the DP array 
-      
+        # 1.) Initialize the DP array 
+        ROWS = len(text1)
+        COLS = len(text2)
+        DP = [[0 for _ in range(COLS+1)] for _ in range(ROWS+1)]
         
-        #2. Initialize with a bunch of 0s for text1.length and text2.length 
-        first = len(text1)
-        second = len(text2)
+        #2.) Loop through everything 
+        for r in range(ROWS,-1,-1):
+            for c in range(COLS, -1, -1):
+                #a. Check if something is extending beyond the index, keep in index 
+                if r == ROWS or c == COLS:
+                    DP[r][c] = 0
+                
+                #b. Check if the index where both characters are matching 
+                elif text1[r] == text2[c]:
+                    DP[r][c] = 1+DP[r+1][c+1]
+                
+                #c. Check if the characters were not matching 
+                elif text1[r] != text2[c]:
+                    DP[r][c] = max(DP[r+1][c], DP[r][c+1])
         
-        #3. initialize everything as 0 
-       
-        dp = [[0 for i in range(second+1)] for j in range(first+1)]
+        return DP[0][0]
         
-        #4. for loop for the actual dp stuff 
-        for i in range(first-1, -1, -1):
-            for j in range(second-1, -1, -1):
-                if text1[i] == text2[j]:
-                    dp[i][j] = dp[i+1][j+1] +1 
-                else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-        return dp[0][0]
         
