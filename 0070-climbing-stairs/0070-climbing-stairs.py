4079235d88e6c13a6ec5073f3d380788d976efea
Harini Karthik <63025647+harinik05@users.noreply.github.com>
2024-09-25 22:11:31 -0400

    Time: 44 ms (7.90%), Space: 16.5 MB (63.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -2,7 +2,24 @@ class Solution:
     def __init__(self):
         #1. Initialize variables 
         self.MEMO = []
+        self.DP = []
     
+    #1. Climb Stairs Function
+    def climbStairs(self, n: int) -> int:
+        # DP array
+        self.DP = ["*"] * (n+1)
+        
+        #base case
+        self.DP[0] = 0
+        self.DP[1] = 1
+        if n>=2:
+            self.DP[2] = 2
+        
+        for i in range(3, n+1):
+            self.DP[i] = self.DP[i-1] + self.DP[i-2]
+        
+        return self.DP[n]
+        
     def memoization(self, i):
         #a. Define some base cases
         if i == 0 or i == 1:
@@ -11,7 +28,7 @@ class Solution:
             self.MEMO[i] = self.memoization(i-1) + self.memoization(i-2)
         return self.MEMO[i]
     #1. Top Down approach (Memoization Approach)
-    def climbStairs(self, n: int) -> int:
+    def climbStairs_MEMO(self, n: int) -> int:
         #a. Define some common rules for memoization 
         self.MEMO = ["*"]*(n+1)
         
