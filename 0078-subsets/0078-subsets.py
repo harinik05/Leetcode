0acd05d73741663c17fe941e2e72826253686b92
Harini <63025647+harinik05@users.noreply.github.com>
2024-10-15 16:06:37 -0400

    Time: 15 ms (68.75%), Space: 11.6 MB (91.69%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    
+    def __init__(self):
+        self.subsetsArr = []
+    def backtrackingFunction(self, index, nums, currentSubset):
+        #pass
+        
+        
+        
+        
+        #b. put everything from the currentsubset into subsetsarr
+        self.subsetsArr.append(currentSubset[:])
+        
+        #c. go through the rest of the elements 
+        for i in range(index, len(nums)):
+            #a. Put in the current subset 
+            currentSubset.append(nums[i])
+            
+            #b. call the backtracking 
+            self.backtrackingFunction(i+1, nums, currentSubset)
+            
+            #c. pop the elemtn 
+            currentSubset.pop()
+        
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.subsetsArr = []
+        self.backtrackingFunction(0,nums,[])
+        return self.subsetsArr
+        
