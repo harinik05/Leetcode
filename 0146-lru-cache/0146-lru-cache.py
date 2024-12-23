9afbaf1e0054d0cda72436d09b3192880788551e
Harini <63025647+harinik05@users.noreply.github.com>
2024-12-23 17:06:25 -0500

    Time: 79 ms (98.99%), Space: 79.2 MB (6.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,109 +1,26 @@
-#Step 1: Create the list node class
-class ListNode():
-    def __init__(self, value):
-        self.value = value 
-        self.prev = None 
-        self.next = None 
+import collections
 
-# Step2: This is your linked list class for the double linked list
-class LRUCache(object):
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.dic = collections.OrderedDict()
 
-    def __init__(self, capacity):
-        """
-        :type capacity: int
-        """
-        self.capacity = capacity 
-        self.head = ListNode((-1,-1)) # Put your LRUs
-        self.tail = ListNode((-1,-1)) # Put your MRUs
-        
-        # establish basic connection
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.hashmap = {} # initialize the key value pair store 
-        self.currCap = 0
-    
-    
-    # Add to the tail 
-    def addNode(self, node):
-        tempNode = node 
-        prevNode = self.tail.prev 
-        prevNode.next = tempNode 
-        tempNode.prev = prevNode 
-        tempNode.next = self.tail 
-        self.tail.prev = tempNode 
-        
-    
-    #Remove from wherever it is (may not even be head )
-    def removeNode(self, node):
-        tempNode = node 
-        prevNode = tempNode.prev
-        nextNode = tempNode.next 
-        prevNode.next = nextNode 
-        nextNode.prev = prevNode
-        
-    
-    def reassignHead(self):
-        headNode = self.head.next 
-        
-        self.removeNode(headNode)
-        k, v = headNode.value
-        del self.hashmap[k]
-        
-        
-    
-    def get(self, key):
-        """
-        :type key: int
-        :rtype: int
-        """
-        # get operation 
-        if key not in self.hashmap:
+    def get(self, key: int) -> int:
+        if key not in self.dic:
             return -1
-        
-        # How to retrieve the value 
-        k, v = self.hashmap[key].value
-        
-        # delete this value 
-        self.removeNode(self.hashmap[key])
-        
-        # add to most recently used 
-        self.addNode(self.hashmap[key])
-        
-        # return the value of the key 
-        return v
-    
 
-    def put(self, key, value):
-        """
-        :type key: int
-        :type value: int
-        :rtype: None
-        """
-        if key in self.hashmap :
-            currentNode = self.hashmap[key]
-            # remove
-            self.removeNode(currentNode)
-            
-            # add
-            self.hashmap[key] = ListNode((key,value))
-            
-            self.addNode(self.hashmap[key])
-            return 
-        
-        if self.currCap == self.capacity:
-            # remove the head 
-            self.reassignHead()
-            self.currCap-=1
-        
-        # add the new node 
-        nodeToAdd = ListNode((key, value))
-        self.addNode(nodeToAdd)
-        self.hashmap[key] = nodeToAdd
+        self.dic.move_to_end(key)
+        return self.dic[key]
 
-        # increment the counter 
-        self.currCap+=1
-        
-            
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.dic.move_to_end(key)
+
+        self.dic[key] = value
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(False)
+
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
