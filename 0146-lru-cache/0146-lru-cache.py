38bf8bbaa7e9d20e296a677bc814870ab4017205
Harini <63025647+harinik05@users.noreply.github.com>
2024-12-23 17:06:59 -0500

    Time: 135 ms (63.27%), Space: 79.5 MB (6.24%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,25 +1,28 @@
 import collections
-
 class LRUCache:
+
     def __init__(self, capacity: int):
+        self.hashmap = collections.OrderedDict()
         self.capacity = capacity
-        self.dic = collections.OrderedDict()
-
+        
     def get(self, key: int) -> int:
-        if key not in self.dic:
-            return -1
-
-        self.dic.move_to_end(key)
-        return self.dic[key]
+        if key not in self.hashmap:
+            return -1 
+        self.hashmap.move_to_end(key)
+        return self.hashmap[key]
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dic:
-            self.dic.move_to_end(key)
-
-        self.dic[key] = value
-        if len(self.dic) > self.capacity:
-            self.dic.popitem(False)
-
+        #1. if the key is in dictionary 
+        if key in self.hashmap:
+            self.hashmap.move_to_end(key)
+        self.hashmap[key]=value
+        #2. check capacity 
+        if len(self.hashmap) > self.capacity:
+            self.hashmap.popitem(last=False)
+        
+        
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
